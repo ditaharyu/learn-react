0a3512b9364e89b7f923bd7c2aba9ef48a60d3fe
Rofiyanto Bagus Pradikta | ꦫꦭꦁ <pradiktabagus@gmail.com>
2023-10-29 00:16:03 +0700

    chore: component Co-authored-by: Anindhita <ditaharyu@users.noreply.github.com>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,37 +3,47 @@ import Logo from "../Assets/Logo.svg"
 import { BsCart2 } from "react-icons/bs"
 import { HiOutlineBars3 } from "react-icons/hi2"
 import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
-import HomeIcon from "@mui/icons-material/Home"
-import InfoIcon from "@mui/icons-material/Info"
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
-import { Phone, WidthFull } from "@mui/icons-material"
+// import HomeIcon from "@mui/icons-material/Home"
+// import InfoIcon from "@mui/icons-material/Info"
+// import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
+// import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
+// import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
+// import { Phone, WidthFull } from "@mui/icons-material"
+import { menuOptions } from "../controller/index.js"
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false)
-  const menuOptions = [
-    {
-      text: "Home",
-      icon: <HomeIcon />
-    },
-    {
-      text: "About",
-      icon: <InfoIcon />
-    },
-    {
-      text: "Testimonial",
-      icon: <CommentRoundedIcon />
-    },
-    {
-      text: "Contact",
-      icon: <PhoneRoundedIcon />
-    },
-    {
-      text: "Cart",
-      icon: <ShoppingCartRoundedIcon />
-    },
-  ]
+
+  // const menuOptions = useMemo(() => {
+  //   return [
+  //     {
+  //       text: "Home",
+  //       icon: <HomeIcon />
+  //     },
+  //     {
+  //       text: "About",
+  //       icon: <InfoIcon />
+  //     },
+  //     {
+  //       text: "Testimonial",
+  //       icon: <CommentRoundedIcon />
+  //     },
+  //     {
+  //       text: "Contact",
+  //       icon: <PhoneRoundedIcon />
+  //     },
+  //     {
+  //       text: "Cart",
+  //       icon: <ShoppingCartRoundedIcon />
+  //     },
+  //   ]
+  // }, [])
+
+  // only for manipulate DOM component
+  // useState => nyimpen state
+  // useEffect => perubahan data
+  // useCallback => function
+  // useLayoutEffect 
 
   return (
     <nav>
@@ -41,10 +51,22 @@ const Navbar = () => {
         <img src={Logo} alt="" />
       </div>
       <div className="navbar-links-container">
-        <a href="">Home</a>
+        {/* v-for="index, items" in menuOptions */}
+        {/* item.title */}
+        {/* const list = [1,2,3,4,5]
+        {list.map((item, i) => (
+          <a href="/" key={i}>{item}</a>
+        ))} */}
+        {
+          menuOptions.map((data, index) => (
+            <a href={data.path} key={index}>{data.text}</a>
+          ))
+        }
+        {/* <a href="">Home</a>
         <a href="">About</a>
+        <a href="">Menu</a>
         <a href="">Testimonials</a>
-        <a href="">Contact</a>
+        <a href="">Contact</a> */}
         <a href="">
           <BsCart2 className="navbar-cart-icon" />
         </a>
@@ -57,19 +79,19 @@ const Navbar = () => {
       </div>
       <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
         <Box
-          sx={{Width: 250}}
+          sx={{ Width: 250 }}
           role="presentation"
           onClick={() => setOpenMenu(false)}
           onKeyDown={() => setOpenMenu(false)}>
           <List>
-            { menuOptions.map((item, i) => (
+            {menuOptions.map((item, i) => (
               <ListItem key={item.text} disablePadding>
                 <ListItemButton>
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
-                </ListItemButton>              
+                </ListItemButton>
               </ListItem>
-            ) )}
+            ))}
           </List>
         </Box>
       </Drawer>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { menuOptions } from './controller';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter(menuOptions);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
